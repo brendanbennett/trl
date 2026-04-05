7557ea1fc97958585ceba45f4c74010ccf6f5b30
Brendan Bennett <brendan.bennett@postgrad.manchester.ac.uk>
2026-04-05 17:40:01 +0100

    Add slurm scripts

--- scripts/posttrain_grpo.sbatch.sh
+++ scripts/posttrain_grpo.sbatch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
-#SBATCH -p gpuL
+#SBATCH -p gpuA
 #SBATCH -G 1
 #SBATCH -t 0-12
 #SBATCH -n 12
@@ -7,12 +7,16 @@
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
 
-echo "Job $SLURM_JOB_ID using $SLURM_NTASKS CPU core(s) on $(hostname)"
-echo "GPUs: $CUDA_VISIBLE_DEVICES"
-
 module purge
 module load libs/cuda
 
-source ~/scratch/trl/.venv/bin/activate
+echo "Job $SLURM_JOB_ID using $SLURM_NTASKS CPU core(s) on $(hostname)"
+echo "GPUs: $CUDA_VISIBLE_DEVICES"
 
-python ~/scratch/trl/scripts/posttrain_grpo.py --per_device_train_batch_size 1 --gradient_accumulation_steps 16
+WORKDIR="\${WORKDIR:-\$HOME/scratch/trl}"
+
+cd "\$WORKDIR"
+
+source .venv/bin/activate
+
+python scripts/posttrain_grpo.py --per_device_train_batch_size 1 --gradient_accumulation_steps 16
--- scripts/posttrain_grpo_h200.sbatch.sh
+++ scripts/posttrain_grpo_h200.sbatch.sh
@@ -1,20 +1,20 @@
 #!/bin/bash --login
-#SBATCH -p gpuH_short
+#SBATCH -p gpuH_short           # up to 1 day
 #SBATCH -G 1
-#SBATCH -t 0-8
+#SBATCH -t 0-12
 #SBATCH -n 8
 #SBATCH -J grpo
-#SBATCH -o ~/scratch/trl/logs/%x_%j.out
-#SBATCH -e ~/scratch/trl/logs/%x_%j.err
-
-echo "Job $SLURM_JOB_ID using $SLURM_NTASKS CPU core(s) on $(hostname)"
-echo "GPUs: $CUDA_VISIBLE_DEVICES"
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
+#SBATCH -A gpu-h200-fse-pgdr    # Account name
 
 module purge
 module load libs/cuda
 
-WORKDIR="\${WORKDIR:-\$HOME/scratch/trl}"
+echo "Job $SLURM_JOB_ID using $SLURM_NTASKS CPU core(s) on $(hostname)"
+echo "GPUs: $CUDA_VISIBLE_DEVICES"
 
+WORKDIR="\${WORKDIR:-\$HOME/scratch/trl}"
 
 cd "\$WORKDIR"
 
